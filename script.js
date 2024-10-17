ed0fc3427e2bb50691b77df42a3e4478754fc3f3
Jayesh Das <jayesh4119@gmail.com>
2024-10-17 16:57:53 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const gridContainer = document.getElementById('grid');
-
-// Create 256 divs (16x16)
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    
-    gridContainer.appendChild(div);
-}
